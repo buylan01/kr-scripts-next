a3c54a61a9033e4e0cb4d4b04c814134b4d06113
buylan <buylan01@gmail.com>
2026-07-24 17:39:05 +0800

    refactor(common): simplify ShellTranslation and remove unused method

--- common/src/main/java/com/omarea/common/shell/ShellTranslation.kt
+++ common/src/main/java/com/omarea/common/shell/ShellTranslation.kt
@@ -7,67 +7,47 @@ import java.util.*
 
 // 从Resource解析字符串，实现输出内容多语言
 class ShellTranslation(val context: Context) {
-    // 示例：
-    // @string:home_shell_01
-    private val regex1 = Regex("^@(string|dimen):[_a-z]+.*", RegexOption.IGNORE_CASE)
-    // 示例
-    // @string/home_shell_01
-    private val regex2 = Regex("^@(string|dimen)/[_a-z]+.*", RegexOption.IGNORE_CASE)
+    private val resources = context.resources
+    private val packageName = context.packageName
 
     fun resolveRow(originRow: String): String {
-        val separator = if (regex1.matches(originRow)) {
-            ':'
-        } else if (regex2.matches(originRow)) {
-            '/'
-        } else {
-            null
-        }
-        if (separator != null) {
-            val row = originRow.trim()
-            val resources = context.resources
-            val type = row.substring(1, row.indexOf(separator)).lowercase(Locale.ENGLISH)
-            val name = row.substring(row.indexOf(separator) + 1)
+        if (!originRow.startsWith("@")) return originRow
 
-            try {
-                val id = resources.getIdentifier(name, type, context.packageName)
-                when (type) {
-                    "string" -> {
-                        return resources.getString(id)
-                    }
-                    "dimen" -> {
-                        return resources.getDimension(id).toString()
-                    }
+        val prefixEnd = originRow.indexOfAny(charArrayOf(':', '/'), startIndex = 1)
+        if (prefixEnd == -1) return originRow
+
+        val type = originRow.substring(1, prefixEnd).lowercase(Locale.ENGLISH)
+        if (type != "string" && type != "dimen") return originRow
+
+        val name = originRow.substring(prefixEnd + 1).trim()
+        val id = resources.getIdentifier(name, type, packageName)
+
+        return try {
+            when (type) {
+                "string" -> {
+                    resources.getString(id)
                 }
-            } catch (ex: Exception) {
-                if (row.contains("[(") && row.contains(")]")) {
-                    return row.substring(row.indexOf("[(") + 2, row.indexOf(")]"))
+                "dimen" -> {
+                    resources.getDimension(id).toString()
                 }
+                else -> originRow
             }
+        } catch (_: Exception) {
+            if (originRow.contains("[(") && originRow.contains(")]")) {
+                originRow.substring(
+                    originRow.indexOf("[(") + 2,
+                    originRow.indexOf(")]")
+                )
+            } else originRow
         }
-
-        return originRow
     }
 
     fun resolveRows(rows: List<String>): String {
         val builder = StringBuilder()
-        var rowIndex = 0
-        for (row in rows) {
-            if (rowIndex > 0) {
-                builder.append("\n")
-            }
+        rows.forEachIndexed { index, row ->
+            if (index > 0) { builder.append("\n") }
             builder.append(resolveRow(row))
-            rowIndex ++
         }
         return builder.toString()
-    }
-
-    fun getTranslatedResult(shellCommand: String, executor: KeepShell?): String {
-        val shell = executor?: KeepShellPublic.getDefaultInstance()
-        val rows = shell.doCmdSync(shellCommand).split("\n")
-        if (rows.isNotEmpty()) {
-            return resolveRows(rows)
-        } else {
-            return ""
-        }
     }
 }
