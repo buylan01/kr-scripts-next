91a39626cd61bb07b7180bd150622865d8cf2713
buylan <buylan01@gmail.com>
2026-07-24 23:31:18 +0800

    refactor(OpenPageHelper): drop unused dialog, simplify toast

--- app/src/main/java/com/krscripts/app/OpenPageHelper.kt
+++ app/src/main/java/com/krscripts/app/OpenPageHelper.kt
@@ -2,36 +2,10 @@ package com.krscripts.app
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Handler
-import android.os.Looper
 import android.widget.Toast
-import com.omarea.common.ui.ProgressBarDialog
 import com.omarea.krscript.model.PageNode
 
 class OpenPageHelper(private var activity: Activity) {
-    private var progressBarDialog: ProgressBarDialog? = null
-    private var handler = Handler(Looper.getMainLooper())
-
-    private val dialog: ProgressBarDialog
-        get() {
-            if (progressBarDialog == null) {
-                progressBarDialog = ProgressBarDialog(activity)
-            }
-            return progressBarDialog!!
-        }
-
-    private fun showDialog(msg: String) {
-        handler.post {
-            dialog.showDialog(msg)
-        }
-    }
-
-    private fun hideDialog() {
-        handler.post {
-            dialog.hideDialog()
-        }
-    }
-
     fun openPage(pageNode: PageNode) {
         try {
             var intent: Intent? = null
@@ -60,7 +34,7 @@ class OpenPageHelper(private var activity: Activity) {
                 activity.startActivity(intent)
             }
         } catch (ex: Exception) {
-            Toast.makeText(activity, "" + ex.message, Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, ex.message, Toast.LENGTH_SHORT).show()
         }
     }
 }
